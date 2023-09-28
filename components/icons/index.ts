31c183f6510efbde7f01682488ed68a05572a2fc
usbangelo <usbangelo@sempreceub.com>
2023-09-28 13:50:38 -0300

    update commit - pages and routing redone

---
+++ components/icons/index.ts
@@ -0,0 +1,61 @@
+import FilesIcon from "./files.svg";
+import MarkdownIcon from "./markdown.svg";
+import TailwindIcon from "./tailwind.svg";
+import LinkIcon from "./link.svg";
+import LightningIcon from "./lightning.svg";
+import GlobeIcon from "./globe.svg";
+import PictureIcon from "./picture.svg";
+import CodeIcon from "./code.svg";
+import BrushIcon from "./brush.svg";
+import DropperIcon from "./dropper.svg";
+import StarsIcon from "./stars.svg";
+import FormulaIcon from "./formula.svg";
+import WarningIcon from "./warning.svg";
+import ChevronRightIcon from "./chevron-right.svg";
+import BoxIcon from "./box.svg";
+import GearIcon from "./gear.svg";
+import RowsIcon from "./rows.svg";
+import CardsIcon from "./cards.svg";
+import OneIcon from "./one.svg";
+import CloudIcon from "./cloud.svg";
+import TableIcon from "./table.svg";
+import FileIcon from "./file.svg";
+import NewsletterIcon from "./newsletter.svg";
+import ArrowRightIcon from "./arrow-right.svg";
+import SwitchIcon from "./switch.svg";
+import TerminalIcon from "./terminal.svg";
+import DiagramIcon from "./diagram.svg";
+import FolderTreeIcon from "./folder-tree.svg";
+import IdCardIcon from "./id-card.svg";
+
+export {
+  FilesIcon,
+  MarkdownIcon,
+  TailwindIcon,
+  LinkIcon,
+  LightningIcon,
+  GlobeIcon,
+  PictureIcon,
+  CodeIcon,
+  BrushIcon,
+  DropperIcon,
+  StarsIcon,
+  FormulaIcon,
+  WarningIcon,
+  ChevronRightIcon,
+  BoxIcon,
+  GearIcon,
+  RowsIcon,
+  CardsIcon,
+  OneIcon,
+  CloudIcon,
+  TableIcon,
+  FileIcon,
+  NewsletterIcon,
+  ArrowRightIcon,
+  SwitchIcon,
+  TerminalIcon,
+  DiagramIcon,
+  FolderTreeIcon,
+  IdCardIcon,
+};
